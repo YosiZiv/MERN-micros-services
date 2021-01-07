6929fdb3241f052c5b7fe83ad05176a6dac43cee
yosi ziv <yosizivjs@gmail.com>
2021-01-07 11:29:31 +0200

    connect to mongodb- create user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 import "express-async-errors";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -18,4 +19,18 @@ app.get("*", () => {
   throw new NotFoundError();
 });
 app.use(errorHandler);
-app.listen(3000, () => console.log("listening on port 3000!!!!!!"));
+
+(async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv: 27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connect to mongoDB");
+  } catch (err) {
+    console.log(err);
+  } finally {
+    app.listen(3000, () => console.log("listening on port 3000!!!!!!"));
+  }
+})();
