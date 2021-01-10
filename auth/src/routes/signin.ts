45f39e5b1b294b92cc36052cdb0df296e0b59579
yosi ziv <yosizivjs@gmail.com>
2021-01-10 23:29:12 +0200

    sign-in flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("test");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("email must be valid"),
+    body("password").trim().isLength({ min: 6, max: 30 }),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await Password.compare(user.password, password))) {
+      throw new BadRequestError("Invalid credentials");
+    }
+    const userJwt = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY!
+    );
+    req.session = { jwt: userJwt };
+    res.status(201).send(user);
+  }
+);
 export { router as signinRouter };
