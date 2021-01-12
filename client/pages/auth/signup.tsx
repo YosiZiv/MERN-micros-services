ddd7a09ccbe8d5e0bfd66e723192ca3e086b8a02
yosi ziv <yosizivjs@gmail.com>
2021-01-12 02:29:56 +0200

    fix few style

--- client/pages/auth/signup.tsx
+++ client/pages/auth/signup.tsx
@@ -1,16 +1,55 @@
-import { Button, Typography, TextField } from "@material-ui/core";
+import {
+  Button,
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Formik, Field, Form, useField, FieldAttributes } from "formik";
+import {
+  Formik,
+  Field,
+  Form,
+  useField,
+  FieldAttributes,
+  FieldArray,
+} from "formik";
 import * as yup from "yup";
+const useStyle = makeStyles({
+  title: {
+    textAlign: "center",
+    padding: "10px",
+  },
+  formContainer: {
+    width: "70%",
+    display: "flex",
+    flexDirection: "column",
+    margin: "0 auto",
+  },
+  input: {
+    padding: "5px",
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  button: {
+    width: "40%",
+    padding: "10px",
+  },
+});
 
 const CustomTextField: React.FC<FieldAttributes<{}>> = ({
   placeholder,
+  className,
   ...props
 }) => {
   const [field, meta] = useField<{}>(props);
   const errorText = meta.touched && meta.error;
   return (
     <TextField
+      className={className}
       placeholder={placeholder}
       {...field}
       helperText={errorText}
@@ -20,10 +59,15 @@ const CustomTextField: React.FC<FieldAttributes<{}>> = ({
 };
 const validationSchema = yup.object({
   email: yup.string().required().email(),
-  password: yup.string().required().min(6).max(256),
+  password: yup.string().required().min(6).max(30),
+  pets: yup.array().of(
+    yup.object({
+      name: yup.string().required(),
+    })
+  ),
 });
 
-export default () => {
+export default function Signup() {
   const classes = useStyle();
   return (
     <div>
@@ -37,6 +81,7 @@ export default () => {
         initialValues={{
           email: "",
           password: "",
+          pets: [{ type: "cat", name: "jarvis", id: Math.random() }],
         }}
         validationSchema={validationSchema}
         onSubmit={async (data, { setSubmitting }) => {
@@ -46,26 +91,68 @@ export default () => {
           setSubmitting(false);
         }}
         validate={(values) => {
+          // custom error handler
           const errors: Record<string, string> = {};
-          if (values.email.includes("test")) {
-            errors.email = "test";
-          }
           return errors;
         }}
       >
         {({ values, errors, touched, isSubmitting }) => (
           <Form>
             <div className={classes.formContainer}>
-              <CustomTextField placeholder="Email" name="email" type="email" />
               <CustomTextField
+                className={classes.input}
+                placeholder="Email"
+                name="email"
+                type="email"
+              />
+              <CustomTextField
+                className={classes.input}
                 placeholder="Password"
                 name="password"
                 type="password"
               />
-
-              <Button type="submit" variant="contained" color="primary">
-                Login
-              </Button>
+              <div className={classes.buttonContainer}>
+                <Button type="submit" variant="contained" color="primary">
+                  Login
+                </Button>
+              </div>
+              {/* <FieldArray name="pets">
+                {(arrayHelpers) => (
+                  <div>
+                    <Button
+                      onClick={() =>
+                        arrayHelpers.push({
+                          id: "" + Math.random(),
+                          name: "",
+                          type: "frog",
+                        })
+                      }
+                    >
+                      Add a pet
+                    </Button>
+                    {values.pets.map((pet, index) => (
+                      <div key={pet.id}>
+                        <CustomTextField
+                          placeholder="Pet name"
+                          name={`pets.${index}.name`}
+                        />
+                        <Field
+                          name={`pets.${index}.type`}
+                          type="select"
+                          as={Select}
+                        >
+                          <MenuItem value="cat">Cat</MenuItem>
+                          <MenuItem value="Dog">Dog</MenuItem>
+                          <MenuItem value="Frog">Frog</MenuItem>
+                        </Field>
+                        <Button onClick={() => arrayHelpers.remove(index)}>
+                          X
+                        </Button>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </FieldArray> */}
             </div>
             <pre>{JSON.stringify(values, null, 2)}</pre>
             <pre>{JSON.stringify(errors, null, 2)}</pre>
@@ -74,15 +161,4 @@ export default () => {
       </Formik>
     </div>
   );
-};
-
-const useStyle = makeStyles({
-  title: {
-    textAlign: "center",
-    padding: "10px",
-  },
-  formContainer: {
-    display: "flex",
-    flexDirection: "column",
-  },
-});
+}
