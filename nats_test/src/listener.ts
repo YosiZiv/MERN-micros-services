654d7734ee92153d585ebb53fab3d81337a75299
yosi ziv <yosizivjs@gmail.com>
2021-01-31 23:53:53 +0200

    subject enum and main

--- nats_test/src/listener.ts
+++ nats_test/src/listener.ts
@@ -1,17 +1,19 @@
-import { clear } from "console";
-import nats, { Message } from "node-nats-streaming";
+import { clear, log } from "console";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreateListener } from "./events/ticket-created-listener";
 clear();
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
-  console.log("Listener connect to nats");
-  const subscription = stan.subscribe("ticket:create");
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-    typeof data === "string" &&
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+  log("Listener connect to nats");
+  stan.on("close", () => {
+    log("Nats connection closed!");
+    process.exit();
   });
+  new TicketCreateListener(stan).listen();
 });
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
